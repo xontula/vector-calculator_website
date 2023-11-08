b724d4fa9a267d7d1f174d577e0e4846e8bd70e9
Amelia Luukkanen <amelia.luukkanen@tuni.fi>
2023-11-08 23:41:37 +0200

    initial frontend files added

---
+++ front/src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+export function App() {
+
+  return (
+    <>
+      <div className="center-content">
+          <table id="main-table">
+            <tbody>
+              <tr>
+                <td>
+                  <div>
+                    <table id="table1"></table>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+    </>
+  )
+}
